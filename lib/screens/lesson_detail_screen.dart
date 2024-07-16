9d4f440c988833b2e0ad09b76b5d289855d5d81b
Lucas Mello22 <lucas.s.mello@outlook.com>
2024-07-15 23:26:33 -0300

    Final commit

--- lib/screens/lesson_detail_screen.dart
+++ lib/screens/lesson_detail_screen.dart
@@ -1,29 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import '../models/topic.dart';
 
-class LessonDetailScreen extends StatelessWidget {
+class LessonDetailScreen extends StatefulWidget {
   final Lesson lesson;
 
   LessonDetailScreen({required this.lesson});
 
   @override
+  _LessonDetailScreenState createState() => _LessonDetailScreenState();
+}
+
+class _LessonDetailScreenState extends State<LessonDetailScreen> {
+  bool isPlaying = false;
+  final TextToSpeech textToSpeech = TextToSpeech();
+
+  void togglePlayPause() async {
+    if (isPlaying) {
+      await textToSpeech.stop();
+    } else {
+      await textToSpeech.speak(widget.lesson.content);
+    }
+    setState(() {
+      isPlaying = !isPlaying;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    textToSpeech.setCompletionHandler(() {
+      setState(() {
+        isPlaying = false;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    textToSpeech.stop();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(lesson.title),
+        title: Text(widget.lesson.title),
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            if (lesson.youtubeUrl != null)
-              YoutubeVideoWidget(youtubeUrl: lesson.youtubeUrl!),
+            if (widget.lesson.youtubeUrl != null)
+              YoutubeVideoWidget(youtubeUrl: widget.lesson.youtubeUrl!),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(
-                lesson.content,
-                style: TextStyle(fontSize: 15.0), // Ajuste o tamanho da fonte aqui
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.lesson.content,
+                    style: TextStyle(fontSize: 15.0),
+                  ),
+                  SizedBox(height: 10.0),
+                  ElevatedButton(
+                    onPressed: togglePlayPause,
+                    child: Text(isPlaying ? 'Parar' : 'Ouvir conteúdo'),
+                  ),
+                ],
               ),
             ),
           ],
@@ -70,18 +116,40 @@ class _YoutubeVideoWidgetState extends State<YoutubeVideoWidget> {
   Widget build(BuildContext context) {
     return _controller != null
         ? YoutubePlayer(
-      controller: _controller,
-      showVideoProgressIndicator: true,
-      progressIndicatorColor: Colors.blueAccent,
-      onReady: () {
-        print('Player is ready.');
-      },
-      onEnded: (metadata) {
-        _controller.seekTo(Duration(seconds: 0));
-      },
-    )
+            controller: _controller,
+            showVideoProgressIndicator: true,
+            progressIndicatorColor: Colors.blueAccent,
+            onReady: () {
+              print('Player is ready.');
+            },
+            onEnded: (metadata) {
+              _controller.seekTo(Duration(seconds: 0));
+            },
+          )
         : Center(
-      child: CircularProgressIndicator(),
-    );
+            child: CircularProgressIndicator(),
+          );
+  }
+}
+
+class TextToSpeech {
+  final FlutterTts flutterTts = FlutterTts();
+
+  TextToSpeech() {
+    flutterTts.setLanguage("pt-BR");
+    flutterTts.setPitch(1.0);
+    flutterTts.setSpeechRate(0.5);
+  }
+
+  Future<void> speak(String text) async {
+    await flutterTts.speak(text);
+  }
+
+  Future<void> stop() async {
+    await flutterTts.stop();
+  }
+
+  void setCompletionHandler(VoidCallback handler) {
+    flutterTts.setCompletionHandler(handler);
   }
 }
